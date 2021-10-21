4e748e22a9ef69d324b88b181281e54c34efd2f5
FabiozZz <fabiozzz@front.dev>
2021-10-21 17:33:43 +0300

    CHORE

--- src/components/GeneralPage/GeneralPage.jsx
+++ src/components/GeneralPage/GeneralPage.jsx
@@ -6,6 +6,7 @@ import ItemCourse from './ItemCourse/ItemCourse';
 import { useSelector } from 'react-redux';
 import { isEmpty } from '../../helpers/common';
 import { notification } from 'antd';
+import { Button } from '../../utils/Buttons/Button';
 
 export const GeneralPage = () => {
 	const generalPage = useSelector(state => state.general_page);
@@ -20,31 +21,38 @@ export const GeneralPage = () => {
 	}, [generalPage.error]);
 
 	useEffect(() => {
-		if (!isEmpty(generalPage.groups)) {
-			setList(
-				generalPage.groups.sort(function (a, b) {
-					return moment(a.date).format('k:mm') > moment(b.date).format('k:mm') ? 1 : -1;
-				})
-			);
-		}
+		// if (!isEmpty(generalPage.groups)) {
+		// 	setList(
+		// 		generalPage.groups.sort(function (a, b) {
+		// 			return moment(a.date).format('k:mm') > moment(b.date).format('k:mm') ? 1 : -1;
+		// 		})
+		// 	);
+		// }
 	}, [generalPage]);
 	return (
 		<>
 			<HeaderNav />
 			<h1 className={classes.title}>Расписание</h1>
 			<div className={classes.wrapper}>
-				<span className={classes.time}>{moment().format('DD.MM.YYYY')}</span>
-				<div className={classes.course_list}>
-					{renderList.length ? (
-						renderList
-							.sort((course, nextCourse) => (course.id > nextCourse.id ? 1 : -1))
-							.map(course => (
-								<ItemCourse key={course.id} course={course} couches={generalPage.couches} />
-							))
-					) : (
-						<h2>Сегодня занятий нет</h2>
-					)}
-				</div>
+				{renderList.length ? (
+					<>
+						<span className={classes.time}>{moment().format('DD.MM.YYYY')}</span>
+						<div className={classes.course_list}>
+							{renderList
+								.sort((course, nextCourse) => (course.id > nextCourse.id ? 1 : -1))
+								.map(course => (
+									<ItemCourse key={course.id} course={course} couches={generalPage.couches} />
+								))}
+						</div>
+					</>
+				) : (
+					<div className={classes.no_lessons}>
+						<span className={classes.no_lessons__text}>Расписание еще не создано</span>
+						<div className={classes.no_lessons__btn}>
+							<Button text={'создать расписание'} factor={'success'} />
+						</div>
+					</div>
+				)}
 			</div>
 		</>
 	);
