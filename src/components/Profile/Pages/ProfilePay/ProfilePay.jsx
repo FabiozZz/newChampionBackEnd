4dce2262faac6423404d0ea0c048d8b85194922a
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2021-11-11 18:49:58 +0300

    CHORE: fuckin chart

--- src/components/Profile/Pages/ProfilePay/ProfilePay.jsx
+++ src/components/Profile/Pages/ProfilePay/ProfilePay.jsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 import { DataPickerRange } from '../../../../utils/DataPickerRange/DataPickerRange';
 import classes from './pay.module.css';
 
-export const ProfilePay = ({profile}) => {
-	const {pay_list} = profile
-	const data = []
+export const ProfilePay = ({ profile }) => {
+	const { pay_list } = profile;
+	const data = [];
 	console.log(profile);
 	const [date /*, setDate*/] = useState({
 		from: '',
@@ -22,176 +22,47 @@ export const ProfilePay = ({profile}) => {
 			<div className={classes.wrapper_result}>
 				<p className={classes.title}>история оплат</p>
 				<div className={classes.wrapper_table}>
-					{pay_list&& pay_list.length?
-					<>
-					<p className={classes.table_caption}>
-						Сумма оплаты за все время: <span className={classes.table_period}>{pay_list.reduce((acc,item)=>acc+= +item.total,0)} &#8381;</span>
-					</p>
-					<table className={classes.table}>
-					   <thead>
-					       <tr>
-					           <th>дата</th>
-					           <th>покупка</th>
-					           <th>сумма</th>
-					           <th >скидка</th>
-					           <th>оплачено</th>
-					           <th>тип оплаты</th>
-					       </tr>
-					   </thead>
-					   <tbody>
-						   {pay_list.map(pay=>{
-							 return (
-								 <tr>
-									 <td>{replaceDateforFront(pay.date)}</td>
-									 <td>{pay.info.rate.name} ({pay.info.client_level.name})</td>
-									 <td>{pay.total} &#8381;</td>
-									 <td>пока нет</td>
-									 <td>{pay.total} &#8381;</td>
-									 <td>{pay.info.payment_method}</td>
-								 </tr>
-							 )  
-						   })}
-					       {/* <tr>
-					           <td>10.02.2021</td>
-					           <td>NONSTOP</td>
-					           <td>Брилиантовый</td>
-					           <td>3200 &#8381;</td>
-					           <td>10%</td>
-					           <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					       </tr>
-					       <tr>
-					           <td>10.03.2021</td>
-					           <td>NONSTOP</td>
-					           <td>Брилиантовый</td>
-					           <td>3200 &#8381;</td>
-					           <td>10%</td>
-					           <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					       </tr>
-					       <tr>
-					            <td>10.04.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.05.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.06.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.07.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.08.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.09.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.10.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.11.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.12.2021</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.01.2022</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.02.2022</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.03.2022</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr>
-					        <tr>
-					            <td>10.03.2022</td>
-					            <td>NONSTOP</td>
-					            <td>Брилиантовый</td>
-					            <td>3200 &#8381;</td>
-					            <td>10%</td>
-					            <td>2 880 &#8381;</td>
-					           <td>Наличные</td>
-					        </tr> */}
-					  </tbody>
-					</table>
-					</>
-					:
-					<span className={classes.mute_text}>Клиент ещё ничего не оплатил</span> 
-				}
+					{pay_list && pay_list.length ? (
+						<>
+							<p className={classes.table_caption}>
+								Сумма оплаты за все время:{' '}
+								<span className={classes.table_period}>
+									{pay_list.reduce((acc, item) => (acc += +item.total), 0).toLocaleString('ru-RU')}{' '}
+									&#8381;
+								</span>
+							</p>
+							<table className={classes.table}>
+								<thead>
+									<tr>
+										<th>дата</th>
+										<th>покупка</th>
+										<th>сумма</th>
+										<th>скидка</th>
+										<th>оплачено</th>
+										<th>тип оплаты</th>
+									</tr>
+								</thead>
+								<tbody>
+									{pay_list.map(pay => {
+										return (
+											<tr>
+												<td>{replaceDateforFront(pay.date)}</td>
+												<td>
+													{pay.info.rate.name} ({pay.info.client_level.name})
+												</td>
+												<td>{Number(pay.total).toLocaleString('ru-RU')} &#8381;</td>
+												<td>пока нет</td>
+												<td>{Number(pay.total).toLocaleString('ru-RU')} &#8381;</td>
+												<td>{pay.info.payment_method}</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</table>
+						</>
+					) : (
+						<span className={classes.mute_text}>Клиент ещё ничего не оплатил</span>
+					)}
 				</div>
 			</div>
 		</>
--- src/components/Reports/Visits/ReportsVisit.jsx
+++ src/components/Reports/Visits/ReportsVisit.jsx
@@ -1,209 +1,230 @@
-// import { Line, Chart } from 'react-chartjs-2';
-// import zoom from 'chartjs-plugin-zoom';
-// import 'chartjs-adapter-moment';
-// import data from './data.json';
-// import 'moment/locale/ru';
-// import moment from 'moment';
-// import { useEffect } from 'react';
-// // Chart.register([
-// //   zoom,
-// //   {
-// //     id: "afterDraw",
-// //     afterDraw: (chart) => {
-// //       console.log(chart);
-// //       // eslint-disable-next-line no-underscore-dangle
-// //       if (chart.tooltip._active && chart.tooltip._active.length) {
-// //         // find coordinates of tooltip
-// //         const activePoint = chart.tooltip._active[0];
-// //         const { ctx } = chart;
-// //         const { x } = activePoint.element;
+import { Line, Chart } from 'react-chartjs-2';
+import zoom from 'chartjs-plugin-zoom';
+import 'chartjs-adapter-moment';
+import data from './data.json';
+import 'moment/locale/ru';
+import moment from 'moment';
+import { useEffect } from 'react';
+// Chart.register([
+//   zoom,
+//   {
+//     id: "afterDraw",
+//     afterDraw: (chart) => {
+//       console.log(chart);
+//       // eslint-disable-next-line no-underscore-dangle
+//       if (chart.tooltip._active && chart.tooltip._active.length) {
+//         // find coordinates of tooltip
+//         const activePoint = chart.tooltip._active[0];
+//         const { ctx } = chart;
+//         const { x } = activePoint.element;
 
-// //         const topY = chart.scales.y;
-// //         const bottomY = chart.scales.y;
+//         const topY = chart.scales.y;
+//         const bottomY = chart.scales.y;
 
-// //         // draw vertical line
-// //         ctx.save();
-// //         ctx.beginPath();
-// //         ctx.moveTo(x, topY);
-// //         ctx.lineTo(x, bottomY);
-// //         ctx.lineWidth = 1;
-// //         ctx.strokeStyle = "#1C2128";
-// //         ctx.stroke();
-// //         ctx.restore();
-// //       }
-// //     }
-// //   }
-// // ]);
-// // Chart.register({
-// //   id: "some",
-// //   afterDraw: function (chart, easing) {
-// //     console.log(chart);
-// //     if (chart.tooltip._active && chart.tooltip._active.length) {
-// //       //   const activePoint = chart.controller.tooltip._active[0];
-// //       //   const ctx = chart.ctx;
-// //       //   const x = activePoint.tooltipPosition().x;
-// //       //   const topY = chart.scales["y-axis-1"].top;
-// //       //   const bottomY = chart.scales["y-axis-1"].bottom;
-// //       //   ctx.save();
-// //       //   ctx.beginPath();
-// //       //   ctx.moveTo(x, topY);
-// //       //   ctx.lineTo(x, bottomY);
-// //       //   ctx.lineWidth = 2;
-// //       //   ctx.strokeStyle = "#e23fa9";
-// //       //   ctx.stroke();
-// //       //   ctx.restore();
-// //     }
-// //   }
-// // });
-// export const ReportsVisit = () => {
-// 	let start_date = moment(data[0].date, 'YYYY-MM-DD');
-// 	let end_date = moment(data[data.length - 1].date, 'YYYY-MM-DD');
+//         // draw vertical line
+//         ctx.save();
+//         ctx.beginPath();
+//         ctx.moveTo(x, topY);
+//         ctx.lineTo(x, bottomY);
+//         ctx.lineWidth = 1;
+//         ctx.strokeStyle = "#1C2128";
+//         ctx.stroke();
+//         ctx.restore();
+//       }
+//     }
+//   }
+// ]);
+// Chart.register({
+//   id: "some",
+//   afterDraw: function (chart, easing) {
+//     console.log(chart);
+//     if (chart.tooltip._active && chart.tooltip._active.length) {
+//       //   const activePoint = chart.controller.tooltip._active[0];
+//       //   const ctx = chart.ctx;
+//       //   const x = activePoint.tooltipPosition().x;
+//       //   const topY = chart.scales["y-axis-1"].top;
+//       //   const bottomY = chart.scales["y-axis-1"].bottom;
+//       //   ctx.save();
+//       //   ctx.beginPath();
+//       //   ctx.moveTo(x, topY);
+//       //   ctx.lineTo(x, bottomY);
+//       //   ctx.lineWidth = 2;
+//       //   ctx.strokeStyle = "#e23fa9";
+//       //   ctx.stroke();
+//       //   ctx.restore();
+//     }
+//   }
+// });
+export const ReportsVisit = () => {
+	const getData = canvas => {
+		const ctx = canvas.getContext('2d');
+		var grad = ctx.createLinearGradient(0, 0, 0, 1000);
 
-// 	let range_min = moment(data[0].date).subtract(10, 'day').format('YYYY-MM-DD'); //start date
+		grad.addColorStop(0.2, 'rgba(67, 191, 65, 0.35)');
+		grad.addColorStop(0.49, 'rgba(67, 191, 65, 0.30)');
+		grad.addColorStop(0.85, 'rgba(67, 191, 65, 0)');
 
-// 	let range_max = moment(data[data.length - 1].date)
-// 		.add(10, 'day')
-// 		.format('YYYY-MM-DD'); //end date
+		return {
+			labels: data.map(e => e.date),
+			datasets: [
+				{
+					data: data.map(e => e.value),
+					fill: true,
+					backgroundColor: grad,
+					pointHitRadius: 15,
+					borderColor: 'rgb(67,191,65)',
+					pointBorderColor: 'transparent',
+					pointBackgroundColor: 'transparent',
+					tension: 0.4,
+				},
+			],
+		};
+	};
 
-// 	useEffect(() => {
-// 		Chart.register(zoom);
-// 	}, []);
-// 	const lineChart = (
-// 		<Line
-// 			data={{
-// 				labels: data.map(e => e.date),
-// 				datasets: [
-// 					{
-// 						data: data.map(e => e.value),
-// 						label: 'First data set',
-// 						borderColor: 'rgb(67,191,65)',
-// 						fill: true,
-// 						tension: 0.5,
-// 					},
-// 				],
-// 			}}
-// 			options={{
-// 				scaleShowVerticalLines: true,
-// 				transitions: {
-// 					zoom: {
-// 						animation: {
-// 							duration: 1000,
-// 							easing: 'easeOutCubic',
-// 						},
-// 					},
-// 				},
-// 				tooltips: {
-// 					responsive: true,
-// 					yAlign: 'center',
-// 				},
-// 				// title: { display: true, text: "My Chart" },
-// 				// maintainAspectRatio: true,
-// 				// responsive: false,
-// 				// interaction: {
-// 				//   mode: "index"
-// 				// },
-// 				elements: {
-// 					point: {
-// 						radius: 0,
-// 					},
-// 					line: {
-// 						borderWidth: 1.5,
-// 					},
-// 				},
-// 				scales: {
-// 					yAxes: {
-// 						max: [...data].map(e => e.value).reduce((acc, val) => (acc > val ? acc : val)) + 100,
-// 						type: 'linear',
-// 						ticks: {
-// 							beginAtZero: true,
-// 						},
-// 						grid: {
-// 							display: false,
-// 						},
-// 					},
-// 					xAxes: {
-// 						// bounds: 'data',
-// 						distribution: 'linear',
-// 						// sampleSize: 1,
-// 						type: 'time',
-// 						time: {
-// 							parser: 'YYYY-MM-DD',
-// 							// unit:'day',
-// 							minUnit: 'day',
-// 							min: start_date,
-// 							max: end_date,
-// 							// unit: "day",
-// 							tooltipFormat: 'DD MMMM YYYY',
-// 							displayFormats: {
-// 								day: 'DD.MM',
-// 								// quarter: "[Q]Q - YYYY",
-// 								month: 'MMMM YYYY',
-// 								year: 'YYYY',
-// 							},
-// 							// stepSize: "1"
-// 						},
-// 						ticks: {
-// 							source:'data',
-// 							autoSkip: true,
-// 							autoSkipPadding: 50,
-// 							maxRotation: 0,
-// 						},
-// 						// ticks: {
-// 						//   source: "labels",
-// 						//   fontSize: 13
-// 						//   // callback: (value, index, values) => {
-// 						//   //   const date = moment(value);
-// 						//   //   return [date.format("M/DD"), date.format("(ddd)")];
-// 						//   // }
-// 						// }
-// 					},
-// 				},
+	let start_date = moment(data[data.length - 30].date, 'YYYY-MM-DD');
+	let end_date = moment(data[data.length - 1].date, 'YYYY-MM-DD');
 
-// 				plugins: {
-// 					legend: {
-// 						display: false,
-// 					},
-// 					tooltip: {
-// 						interaction: {
-// 							intersect: false,
-// 						},
-// 					},
-// 					zoom: {
-// 						pan: {
-// 							enabled: true,
-// 							mode: 'x',
-// 							// modifierKey: "alt",
-// 							rangeMin: {
-// 							  x: range_min
-// 							},
-// 							rangeMax: {
-// 							  x: range_max
-// 							},
-// 							sensitivity: 0.25,
-// 							// threshold: 22
-// 						},
-// 						limits: {
-// 							// x: { min: range_min, max: "original" }
-// 						},
-// 						zoom: {
-// 							wheel: {
-// 								enabled: true,
-// 								speed: 0.4,
-// 							},
-// 							mode: 'x',
-// 							threshold: 10,
-// 							rangeMin: {
-// 							  x: range_min
-// 							},
-// 							rangeMax: {
-// 							  x: range_max
-// 							}
-// 						},
-// 					},
-// 				},
-// 			}}
-// 		/>
-// 	);
+	let range_min = moment(data[0].date).subtract(10, 'day').format('YYYY-MM-DD'); //start date
 
-// 	return <div className="gcol-12">{lineChart}</div>;
-// };
+	let range_max = moment(data[data.length - 1].date)
+		.add(10, 'day')
+		.format('YYYY-MM-DD'); //end date
+
+	useEffect(() => {
+		Chart.register(zoom);
+	}, []);
+	const lineChart = (
+		<Line
+			data={getData}
+			options={{
+				scaleShowVerticalLines: true,
+				transitions: {
+					zoom: {
+						animation: {
+							duration: 1000,
+							easing: 'easeOutCubic',
+						},
+					},
+				},
+				// tooltips: {
+				// 	responsive: true,
+				// 	yAlign: 'center',
+				// },
+
+				// title: { display: true, text: "My Chart" },
+				// maintainAspectRatio: true,
+				// responsive: false,
+				// interaction: {
+				//   mode: "index"
+				// },
+				elements: {
+					point: {
+						radius: 0,
+					},
+					line: {
+						borderWidth: 1.5,
+					},
+				},
+				scales: {
+					yAxes: {
+						max: [...data].map(e => e.value).reduce((acc, val) => (acc > val ? acc : val)) + 100,
+						type: 'linear',
+						ticks: {
+							beginAtZero: true,
+						},
+						grid: {
+							display: false,
+						},
+					},
+					xAxes: {
+						// bounds: 'data',
+						distribution: 'linear',
+						// sampleSize: 1,
+						type: 'time',
+						min: start_date,
+						max: end_date,
+						time: {
+							parser: 'YYYY-MM-DD',
+							// unit:'day',
+							minUnit: 'day',
+							// unit: 'day',
+							tooltipFormat: 'DD MMMM YYYY',
+							displayFormats: {
+								day: 'DD.MM',
+								// quarter: "[Q]Q - YYYY",
+								month: 'MMMM YYYY',
+								year: 'YYYY',
+							},
+							// stepSize: "1"
+						},
+						ticks: {
+							// source: 'data',
+							autoSkip: true,
+							autoSkipPadding: 20,
+							maxRotation: 0,
+						},
+						// ticks: {
+						//   source: "labels",
+						//   fontSize: 13
+						//   // callback: (value, index, values) => {
+						//   //   const date = moment(value);
+						//   //   return [date.format("M/DD"), date.format("(ddd)")];
+						//   // }
+						// }
+						callbacks: {
+							afterFit: function (some) {
+								console.log(some);
+							},
+						},
+					},
+				},
+
+				plugins: {
+					legend: {
+						display: false,
+					},
+					tooltip: {
+						interaction: {
+							intersect: false,
+						},
+						displayColors: false,
+					},
+					zoom: {
+						pan: {
+							enabled: true,
+							mode: 'x',
+							// modifierKey: "alt",
+							rangeMin: {
+								x: range_min,
+							},
+							rangeMax: {
+								x: range_max,
+							},
+							sensitivity: 0.25,
+							// threshold: 22
+						},
+						limits: {
+							// x: { min: range_min, max: "original" }
+						},
+						zoom: {
+							wheel: {
+								enabled: true,
+								speed: 0.4,
+							},
+							mode: 'x',
+							threshold: 10,
+							rangeMin: {
+								x: range_min,
+							},
+							rangeMax: {
+								x: range_max,
+							},
+						},
+					},
+				},
+			}}
+		/>
+	);
+
+	return <div className="gcol-12">{lineChart}</div>;
+};
